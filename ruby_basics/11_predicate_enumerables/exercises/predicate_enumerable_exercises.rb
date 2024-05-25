d9ca0b84452be8ed9d038f952d495525c92e3649
Steve <stevewu24@gmail.com>
2024-05-24 22:47:05 -0700

    finished assignment for ruby for the odin project

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -1,19 +1,62 @@
+require 'pry-byebug'
+
 def coffee_drink?(drink_list)
   # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+  drink_list.include?("coffee") || drink_list.include?("espresso")
 end
 
 def correct_guess?(guess_list, answer)
   # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+  guess_list.any? do |item|
+    if item == answer
+      return true
+    end
+  end
+  false
 end
 
 def twenty_first_century_years?(year_list)
   # use #all? to return true when all of the years in the year_list (array) are between 2001 and 2100
+  year_list.all? do |year|
+    year.between?(2001, 2100)
+  end
 end
 
 def correct_format?(word_list)
   # use #none? to return true when none of the words in the word_list (array) are in upcase
+  word_list.none? do |word|
+    if word.upcase == word
+      # return false if they all have upcase in them
+      return false
+    end
+  end
+  #binding.pry
+  true
 end
 
 def valid_scores?(score_list, perfect_score)
   # use #one? to return true when only one value in the score_list (hash) is equal to the perfect_score (number)
+  score_list.one? { |key, val| val == perfect_score }
 end
+
+=begin 
+my way of doing this first try
+
+def valid_scores?(score_list, perfect_score)
+  # use #one? to return true when only one value in the score_list (hash) is equal to the perfect_score (number)
+  duplicates = []
+
+  if score_list.empty?
+    return false
+  end
+
+  score_list.one? do |key, val|
+    if duplicates.include?(val)
+      return false
+    elsif val == perfect_score 
+      duplicates.push(val) 
+    end
+  end
+  true
+end 
+=end
---
+++ script.rb
@@ -0,0 +1,14 @@
+require 'pry-byebug'
+
+
+def two_sum(nums, target)
+  result = []
+  nums.each_with_index do |item, index| 
+    puts item
+  end
+  # return index
+  result
+end
+
+nums = [2,7,11,15], target = 9
+two_sum(nums, target)
